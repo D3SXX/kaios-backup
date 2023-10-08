35e1b9408d224d0e5c2d2b5f40ebce8b1ee1ea07
Vlad R <75074588+D3SXX@users.noreply.github.com>
2023-10-08 12:47:14 +0300

    Update code
    UI now is in mostly functional state

--- src/index.js
+++ src/index.js
@@ -2,8 +2,65 @@
 //import menuUpdater from "./js/menuUpdater";
 
 document.activeElement.addEventListener('keydown', handleKeydown);
+var key;
 var row = 0;
+var col = 1;
 var rowLimit;
+var currentDate;
+var filename;
+var holdIndex = ["SMS","MMS","Logs"];
+var holdValues = [false,false,false];
+
+function refreshDate(){
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  currentDate = `${day}-${month}-${year}`;
+}
+function showDebug(){
+  const debugElement = document.getElementById('debug');
+  debugElement.innerHTML = `nav: ${key} row: ${row} (${rowLimit}) col: ${col}`;
+}
+
+function isElementInFocus(element) {
+  return element === document.activeElement;
+}
+
+function focusInput(id) {
+  const inputElement = document.getElementById("i" + id);
+  const isInFocus = isElementInFocus(inputElement);
+  if (!isInFocus){
+  if (inputElement) {
+    inputElement.focus();
+    inputElement.value = "";
+    console.log('id: i' +  row + ' - focused');
+  }
+}
+else{
+  const inputValue = inputElement.value;
+  filename = inputValue;
+  inputElement.blur();
+  console.log('id: i' +  row + ' - unfocused');
+}
+console.log('filename is set to: ' + filename);
+}
+
+function check(id) {
+  const checkbox = document.getElementById("b" + id);
+  if (checkbox.checked) {
+    checkbox.checked = false;
+    holdValues[id-1] = false;
+    console.log('id: b' +  row + ' - unchecked');
+  }
+  else{
+    checkbox.checked = true;
+    holdValues[id-1] = true;
+    console.log('id: b' +  row + ' - checked');
+  }
+  console.log('Values: ' +  holdValues);
+}
+
 
 function handleKeydown(e) {
   switch (e.key) {
@@ -23,10 +80,15 @@ function handleKeydown(e) {
       nav(4);
       console.log("ArrowLeft triggered")
       break;
+    case 'Enter':
+      nav(5);
+      console.log("Enter triggered");
+      break;
   }
 }
 
 function nav(move) {
+  key = move;
   const currentIndex = document.activeElement.tabIndex;
   const next = currentIndex + move;
   const items = document.querySelectorAll('.items');
@@ -34,8 +96,7 @@ function nav(move) {
   if (targetElement) {
     targetElement.focus();
   }
-    const debugElement = document.getElementById('debug');
-    debugElement.innerHTML = `nav: ${move} row: ${row} (${rowLimit})`;
+    
     updateMenuContainer(move);
 }
 
@@ -51,16 +112,24 @@ function updateMenuContainer(nav) {
   if (nav == 3 || nav == 4) {
     row = 0;
     if (nav == 4) {
+      col = 1;
       newEntry = `<ul>
-      <li id = 1>Save SMS</li>
-      <li id = 2>Save MMS</li>
-      <li id = 3>Save Logs</li>
+      <li id = 1>Save SMS <input type="checkbox" id="b1" name="SMS"></li>
+      <li id = 2>Save MMS <input type="checkbox" id="b2" name="MMS"></li>
+      <li id = 3>Save Logs <input type="checkbox" id="b3" name="LOGS"></li>
       </ul>`;
       navbarEntry = '<label>Data Selection</label>'
       rowLimit = 3;
     } else if (nav == 3) {
-      newEntry = ` <div class="input"><input type="text" id="1" nav-selectable="true" autofocus />
-      <label>Filename:</label></div><br>`;
+      col = 2;
+      refreshDate();
+      if (!filename){
+        filename = "backup_" + currentDate;
+      }
+      
+      newEntry = ` <ul>
+      <li id = 1>Filename: <input type="text" id="i1" value=${filename} nav-selectable="true" autofocus /></li>
+      </ul>`;
       navbarEntry = '<label>Filenames</label>'
       rowLimit = 1;
     }
@@ -70,8 +139,8 @@ function updateMenuContainer(nav) {
       //menuContainer.innerHTML += newEntry2;
       navbar.innerHTML = navbarEntry;
     }
-  } else {
-    if (row) {
+  } else if (nav == 1 || nav == 2) {
+    if (row && row <= rowLimit) {
       const pastElement = document.getElementById(row);
       pastElement.classList.remove('hovered');
     }
@@ -88,7 +157,7 @@ function updateMenuContainer(nav) {
         row = 0;
       }
     } else {
-      if (row > 1) {
+      if (row >= 1) {
         row--;
         const element = document.getElementById(row);
         if (element) {
@@ -97,8 +166,24 @@ function updateMenuContainer(nav) {
         }
       }
       else{
-        row = rowLimit;
+        row = rowLimit+1;
       }
     }
   }
+  else{
+    if (col == 1){
+      check(row);
+    }
+    else{
+      focusInput(row);
+    }
+
+
+  
+  
 }
+showDebug();
+}
+
+
+nav(4);
