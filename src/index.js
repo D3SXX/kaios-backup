6badcd46e33b68f07b39ff311dd552941f0a2d85
D3SXX <9647752d@gmail.com>
2023-10-13 18:02:11 +0300

    Update code

--- src/index.js
+++ src/index.js
@@ -10,52 +10,112 @@ var currentDate;
 var holdIndex = ["SMS", "MMS", "Logs"];
 var holdValues = [false, false, false];
 refreshDate();
-var filename = "backup_" + currentDate;
+var filename = "KaiOS_Backup/backup_" + currentDate + "/backup_" + currentDate;
+var enableDebug = false;
 
-function writeToFile(array, amount, filename){
+function writeToFile(array, amount, filename, type, format){
   let plainText = "";
-  filename = filename + ".txt"
-  console.log("Trying to upload " + amount + " elements to " + filename);
-  for(let i = 0; i<amount; i++){
-    plainText += "type: " + array[i].type;
-    plainText += " id: " + array[i].id;
-    plainText += " threadId: " + array[i].threadId;
-    plainText += " iccId: " + array[i].iccId;
-    plainText += " deliveryStatus: " + array[i].deliveryStatus;
-    plainText += " sender: " + array[i].sender;
-    plainText += " receiver: " + array[i].receiver;
-    plainText += " body: " + array[i].body;
-    plainText += " messageClass: " + array[i].messageClass;
-    plainText += "\n";  
+  let oldFilename = filename;
+  console.log("Trying to upload " + amount + " elements (" + type + ") to " + filename + "(" + format + ")");
+  switch (format){
+    case 'plain':
+      
+      switch (type){
+        case 'sms':
+          filename = filename + "_SMS"
+      for(let i = 0; i<amount; i++){
+        plainText += "type: " + array[i].type;
+        plainText += " id: " + array[i].id;
+        plainText += " threadId: " + array[i].threadId;
+        plainText += " iccId: " + array[i].iccId;
+        plainText += " deliveryStatus: " + array[i].deliveryStatus;
+        plainText += " sender: " + array[i].sender;
+        plainText += " receiver: " + array[i].receiver;
+        plainText += " body: " + array[i].body;
+        plainText += " messageClass: " + array[i].messageClass;
+        plainText += " deliveryTimestamp: " + array[i].deliveryTimestamp;
+        plainText += " read: " + array[i].read;
+        plainText += " sentTimestamp: " + array[i].sentTimestamp;
+        plainText += " timestamp: " + array[i].timestamp;
+        plainText += "\n";  
 
-  }
-  var sdcard = navigator.getDeviceStorage("sdcard");
-  var oMyBlob = new Blob([plainText], { "type" : "text/plain" });
-  var request = sdcard.addNamed(oMyBlob, filename);
+      }
+      break;
+      case 'mms':
+        filename = filename + "_MMS"
+        for (let i = 0; i < amount; i++) {
+          const message = array[i];
+  
+          plainText += "type: " + message.type;
+          plainText += " id: " + message.id;
+          plainText += " threadId: " + message.threadId;
+          plainText += " iccId: " + message.iccId;
+          plainText += " delivery: " + message.delivery;
+          plainText += " expiryDate: " + message.expiryDate;
+          plainText += " attachments: " + message.attachments.join(', ');
+          plainText += " read: " + message.read;
+          plainText += " readReportRequested: " + message.readReportRequested;
+          plainText += " receivers: " + message.receivers.join(', '); 
+          plainText += " sentTimestamp: " + message.sentTimestamp;
+          plainText += " smil: " + message.smil;
+          plainText += " subject: " + message.subject;
+          plainText += " timestamp: " + message.timestamp;
+          plainText += "\n";
+        }
+        break;
 
+    }
+      filename = filename + ".txt"
+      var sdcard = navigator.getDeviceStorage("sdcard");
+      var oMyBlob = new Blob([plainText], { "type" : "text/plain" });
+      var request = sdcard.addNamed(oMyBlob, filename);
+      break;
+      }
   request.onsuccess = function() {
-    alert('Data was succesfully writen to the internal storage (' + filename + ')');
+    alert('Data was successfully written to the internal storage (' + filename + ')');
   }
   request.onerror = function() {
-    console.log('Error hapenned while trying to write to ' + filename)
-    alert('Error hapenned while trying to write to ' + filename + ' ' + this.error);
+    console.log('Error happened while trying to write to ' + oldFilename)
+    alert('Error happened while trying to write to ' + filename + ' ' + this.error);
   }
 
 }
-function SMSMessage(type, id, threadId, iccId, delivery, deliveryStatus, sender, receiver, body, messageClass) {
-  this.type = type;
-  this.id = id;
-  this.threadId = threadId;
-  this.iccId = iccId;
-  this.delivery = delivery;
-  this.deliveryStatus = deliveryStatus;
-  this.sender = sender;
-  this.receiver = receiver;
-  this.body = body;
-  this.messageClass = messageClass;
+function SMSMessage(message) {
+  this.type = message.type || '';
+  this.id = message.id || '';
+  this.threadId = message.threadId || '';
+  this.iccId = message.iccId || '';
+  this.delivery = message.delivery || '';
+  this.deliveryStatus = message.deliveryStatus || '';
+  this.sender = message.sender || '';
+  this.receiver = message.receiver || '';
+  this.body = message.body || '';
+  this.messageClass = message.messageClass || '';
+  this.deliveryTimestamp = message.deliveryTimestamp || '';
+  this.read = message.read || false;
+  this.sentTimestamp = message.sentTimestamp || '';
+  this.timestamp = message.timestamp || '';
 }
 
+function MMSMessage(message) {
+  this.type = message.type || '';
+  this.id = message.id || '';
+  this.threadId = message.threadId || '';
+  this.iccId = message.iccId || '';
+  this.delivery = message.delivery || '';
+  this.deliveryInfo = message.deliveryInfo || {};
+  this.expiryDate = message.expiryDate || '';
+  this.attachments = message.attachments || [];
+  this.read = message.read || false;
+  this.readReportRequested = message.readReportRequested || false;
+  this.receivers = message.receivers || [];
+  this.sentTimestamp = message.sentTimestamp || '';
+  this.smil = message.smil || '';
+  this.subject = message.subject || '';
+  this.timestamp = message.timestamp || '';
+}
 const smsMessages = [];
+const mmsMessages = [];
 
 function refreshDate() {
   const date = new Date();
@@ -65,8 +125,10 @@ function refreshDate() {
   currentDate = `${day}-${month}-${year}`;
 }
 function showDebug() {
+  if(enableDebug){
   const debugElement = document.getElementById("debug");
   debugElement.innerHTML = `nav: ${key} row: ${row} (${rowLimit}) col: ${col}`;
+  }
 }
 
 function isElementInFocus(element) {
@@ -131,6 +193,10 @@ function handleKeydown(e) {
       nav(6);
       console.log("SoftRight triggered");
       break;
+    case "#":
+      console.log("# key triggered");
+      enableDebug = true;
+      break;
   }
 }
 
@@ -147,66 +213,115 @@ function nav(move) {
   updateMenuContainer(move);
 }
 
-function fetchSMSMessages(settings) {
-  if (settings.every((element) => element === false)) {
-    console.error("Nothing was selected to backup");
-    alert("Nothing was selected to backup");
-    return;
-  } else {
-    console.log("Starting backup " + settings);
-    let smsManager =
-      window.navigator.mozSms || window.navigator.mozMobileMessage;
-    if (!smsManager) {
-      console.error("Could not get API access");
-      alert("Could not get API access");
-      return;
-    }
-    console.log("Got access to mozSms or mozMobileMessage");
-    let request = smsManager.getMessages(null, false);
-    if (!request) {
-      console.error("Coult not access getMessages().");
-      alert("Coult not access getMessages().");
-      return;
-    }
-    console.log("Got access to getMessages(), starting scan");
-    let amount = 0;
-    request.onsuccess = function () {
-      let cursor = request;
-      if (!cursor.result) {
-        console.log("Got the last message");
-        console.log("Succesfully scanned " + amount + " messages.");
-        writeToFile(smsMessages, amount, filename);
-        
+function fetchSMSMessages() {
+      console.log("fetchSMSMessages: Starting backup");
+      let smsManager = window.navigator.mozSms || window.navigator.mozMobileMessage;
+      if (!smsManager) {
+        console.error("Could not get API access");
+        alert("Could not get API access");
         return;
       }
-      const message = cursor.result;
-      if (message) {
-        const newMessage = new SMSMessage(
-          message.type,
-          message.id,
-          message.threadId,
-          message.iccId,
-          message.delivery,
-          message.deliveryStatus,
-          message.sender,
-          message.receiver,
-          message.body,
-          message.messageClass
-        );
-      
-        smsMessages.push(newMessage);
-        amount += 1;
-        cursor.continue();
-      } else {
-        console.log("No more messages");
+      console.log("Got access to mozSms or mozMobileMessage");
+      let request = smsManager.getMessages(null, false);
+      if (!request) {
+        console.error("Couldn't access getMessages().");
+        alert("Couldn't access getMessages().");
+        return;
       }
-    };
-    request.onerror = function () {
-      console.error("Error accessing SMS messages: " + request.error.name);
-      alert("Error accessing SMS messages.");
-    };
+      console.log("Got access to getMessages(), starting scan");
+      let amount = 0;
+      request.onsuccess = function () {
+        let cursor = request;
+        if (!cursor.result) {
+          console.log("Got the last message");
+          console.log("Successfully scanned " + amount + " messages.");
+          writeToFile(smsMessages, amount, filename, "sms", "plain");
+          return;
+        }
+        const message = cursor.result;
+        if (message.type == "sms") {
+          const newMessage = new SMSMessage(message); // Create SMSMessage from message object
+          smsMessages.push(newMessage);
+          amount += 1;
+          cursor.continue();
+        } else {
+          console.log("Not an SMS message, skipping...");
+          cursor.continue();
+        }
+      };
+      request.onerror = function () {
+        console.error("Error accessing SMS messages: " + request.error.name);
+        alert("Error accessing SMS messages.");
+      };
     
+}
+
+
+function fetchMMSMessages() {
+  console.log("fetchMMSMessages: Starting backup");
+
+  let mmsManager = window.navigator.mozMms || window.navigator.mozMobileMessage;
+
+  if (!mmsManager) {
+    console.error("Could not get MMS API access");
+    alert("Could not get MMS API access");
+    return;
   }
+
+  let request = mmsManager.getMessages(null, false);
+  let amount = 0;
+
+  request.onsuccess = function () {
+    let cursor = request;
+    if (!cursor.result) {
+      console.log("Got the last MMS message");
+      console.log("Successfully scanned " + amount + " MMS messages.");
+      writeToFile(mmsMessages, amount, filename, "mms", "plain");
+      saveMMSImages(mmsMessages);
+      return;
+    }
+    const message = cursor.result;
+    if (message.type == "mms") {
+      const newMessage = new MMSMessage(message);
+      mmsMessages.push(newMessage);
+      amount += 1;
+      cursor.continue();
+    } else {
+      console.log("Not an MMS, skipping...");
+      cursor.continue();
+    }
+    
+  };
+
+  request.onerror = function () {
+    console.error("Error accessing MMS messages: " + request.error.name);
+    alert("Error accessing MMS messages.");
+  };
+
+}
+
+function saveMMSImages(mmsMessages) {
+  for (let i = 0; i < mmsMessages.length; i++) {
+    const attachments = mmsMessages[i].attachments;
+    for (let j = 0; j < attachments.length; j++) {
+      const attachment = attachments[j];
+      const imageFilename = 'KaiOS_Backup/backup_' + currentDate +  '/MMS_images/' + attachment.location;
+      const imageUrl = attachment.content;
+      saveImageToFile(imageUrl, imageFilename);
+    }
+  }
+}
+
+function saveImageToFile(imageUrl, filename) {
+  const sdcard = navigator.getDeviceStorage('sdcard');
+  const blob = new Blob([imageUrl]);
+  const request = sdcard.addNamed(blob, filename);
+  request.onsuccess = function () {
+    console.log('Image saved as ' + filename);
+  };
+  request.onerror = function () {
+    console.error('Error while saving image: ' + request.error.name);
+  };
 }
 
 
@@ -239,7 +354,7 @@ function updateMenuContainer(nav) {
       col = 2;
       refreshDate();
       if (!filename) {
-        filename = "backup_" + currentDate;
+        filename = "KaiOS_Backup/backup_" + currentDate + "/backup_" + currentDate;
       }
 
       newEntry = ` <ul>
@@ -289,7 +404,19 @@ function updateMenuContainer(nav) {
       focusInput(row);
     }
   } else {
-    fetchSMSMessages(holdValues);
+    if (holdValues.every((element) => element === false)) {
+      console.error("Nothing was selected to backup");
+      alert("Nothing was selected to backup");
+    }
+    else{
+      if (holdValues[0] == true){
+        fetchSMSMessages();
+    }
+    if(holdValues[1] == true){
+      fetchMMSMessages();
+    }
+  }
+    
   }
 
   showDebug();
